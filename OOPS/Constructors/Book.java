11fc020b91b33a9e6be8d5f570092c85c9138d0c
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2025-06-24 12:04:19 +0530

    [Dinesh] Add . Book

---
+++ OOPS/Constructors/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    // Default Constructor
+    public Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
+    }
+
+    // Parameterized Constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
