8bb54c00192f02fcdc520d47c4af3743dc1fc3fc
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2025-06-24 12:08:27 +0530

    [Dinesh Sirsiya] Add . Hotel Booking System

---
+++ OOPS/Constructors/HotelBooking.java
@@ -0,0 +1,26 @@
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default Constructor
+    public HotelBooking() {
+        this.guestName = "Guest";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    // Parameterized Constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy Constructor
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+}
